48eb16a6577970609a1359488dbb3b8a40853f67
daniele <danieleambrosino97@gmail.com>
2016-08-13 17:13:14 +0200

    Prepared project GUI with Qt5 Widgets library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <QApplication>
+#include <QDesktopWidget>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+#include "gui/mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  MainWindow w;
+
+  QRect screenGeometry = a.desktop()->screenGeometry();
+  int x = (screenGeometry.width() - w.width()) / 2;
+  int y = (screenGeometry.height() - w.height()) / 2;
+  w.move(x, y);
+
+  w.show();
+  return a.exec();
 }
