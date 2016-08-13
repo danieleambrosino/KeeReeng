c198d11ee95401a9084a0325343cc268da9cb212
daniele <danieleambrosino97@gmail.com>
2016-08-13 03:34:02 +0200

    Defined Database c'tor and d'tor

--- src/core/Database.cpp
+++ src/core/Database.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Database.h"
+
+#include <QFile>
+#include <QtCore/QCryptographicHash>
+
+Database::Database(const QString &filename, const QString &password) :
+    file(new QFile(filename)),
+    m_key(QCryptographicHash::hash(password, QCryptographicHash::Md5)) {}
+
+Database::~Database() {
+  delete file;
+}
