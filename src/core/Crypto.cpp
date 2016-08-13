1f6aebe94b8d036206eb0fc20d169b017b2d2bd9
daniele <danieleambrosino97@gmail.com>
2016-08-13 02:55:31 +0200

    Define Crypto::encrypt method

--- src/core/Crypto.cpp
+++ src/core/Crypto.cpp
@@ -7,8 +7,28 @@
 #include <QString>
 #include <QCryptographicHash>
 
+#define MD5_SIZE  16
+#define SHA1_SIZE 20
+
 Crypto::Crypto(const QString &password) :
     m_key(QCryptographicHash::hash(password.toUtf8(),
                                    QCryptographicHash::Md5)) {}
+
+QByteArray Crypto::encrypt(const QByteArray &bin) {
+  QByteArray tmp = bin;
+
+  tmp.prepend(QCryptographicHash::hash(bin, QCryptographicHash::Sha1));
+
+  int pos(0), size(bin.size());
+  char last(0);
+
+  while (pos < size) {
+    tmp[pos] = tmp.at(pos) ^ m_key.at(pos % MD5_SIZE) ^ last;
+    last = tmp.at(pos);
+    ++pos;
+  }
+
+  return tmp;
+}
 
 
